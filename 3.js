3c1c60ce347afe9378424e854fbfc84ff3c5e7b2
sofyan-dev <mchsfyan17@gmail.com>
2020-08-10 06:59:28 +0700

    final

--- 3.js
+++ 3.js
@@ -1,15 +1,12 @@
-var printSegitiga = 5;
-let x = printSegitiga;
-
-if(typeof x !== 'number'){
-  console.log("Input Harus Number!!")
-  return false;
-}
-
-for (let i = x; i >= 1; i--) {
-  let num = '';
-	for (let j = 1; j <= i; j++) {
-		num += j;
-	}
-	console.log(num);
-}
+printSegitiga = (x) => {
+	if(typeof x !== 'number') { console.log("Input Harus Number!!") //validasi
+  	return false }
+	for (let i = x; i >= 1; i--) { //loop
+		  let num = ''
+			for (let j = 1; j <= i; j++) {
+				num += j
+			}
+			console.log(num)
+		}
+	} 
+printSegitiga(4)
